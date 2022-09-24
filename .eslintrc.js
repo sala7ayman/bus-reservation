a3d294001b999503c2caff65c45995db350e805f
salah ayman <sala7ayman@gmail.com>
2022-09-24 18:13:59 +0200

    add eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 13,
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint','prettier'],
+    rules: {
+      'prettier/prettier': 2,
+      quotes: ['error', 'single'],
+      'no-console': 1,
+      'no-var': 'error',
+      'prefer-const': 'error',
+    },
+  };
